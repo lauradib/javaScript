8f4baf977052620472be271beb29a12326dee841
lauradib <diblaura22@gmail.com>
2024-04-18 09:54:58 -0300

    javascript

---
+++ js/scrip.js
@@ -0,0 +1,15 @@
+console.log("Olá mundo")
+
+// caixas de texto (input)
+// prompt = recebe um texto
+//window.prompt("Qual é seu nome?");
+
+//alert = manda uma mensagem
+//window.alert("Front é top");
+
+// confirm = persunta podendo sair verdadeira ou falsa
+//window.confirm("Deseja baixar o arquivo novamente?");
+
+var melancia = "Uma variavel teste";
+var ligado = true;
+var numero= 98;
